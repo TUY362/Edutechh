b11e7766971584fb25800e683d98895586c337c4
kevin tuy <ktuy-2026320@kinal.edu.gt>
2026-05-30 01:54:05 +0200

    001 se agrego el model

---
+++ src/org/kevintuy/model/conexion/Conexion.java
@@ -0,0 +1,26 @@
+package org.kevintuy.model.conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+    private static final String URL = "jdbc:mysql://localhost:3306/EduTechdb_In4cm?serverTimezone=UTC";
+    private static final String USER = "root";
+    private static final String PASSWORD = "09012011";
+
+    public static Connection conectar() {
+        Connection conexion = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conexion = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (ClassNotFoundException e) {
+            System.out.println("ERROR: No se encontró el driver de MySQL.");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println("ERROR: Fallo en la conexión a la base de datos.");
+            e.printStackTrace();
+        }
+        return conexion;
+    }
+}
